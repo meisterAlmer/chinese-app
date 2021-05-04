c1cc71f9e0c517c7be333cd756629468e9a9bb83
Almer <48512743+meisterAlmer@users.noreply.github.com>
2021-05-04 23:18:19 +0200

    word list data filters

--- src/components/newWords/NewWords.js
+++ src/components/newWords/NewWords.js
@@ -1,26 +1,82 @@
 import './NewWords.css';
+import { useState, useEffect } from 'react';
 
 function NewWords({ data }) {
+  const [sortData, setSortData] = useState(data);
+  const [filterType, toggleFilterType] = useState('type-ab');
+
+  const sortType = function (sort) {
+    toggleFilterType(filterType === `${sort}-ab` ? `${sort}-ba` : `${sort}-ab`);
+  };
+
+  const sortArray = function (arr, sort, order) {
+    const myArr = arr;
+    if (order === 'ab') {
+      myArr.sort((a, b) => a[sort].localeCompare(b[sort]));
+    }
+    if (order === 'ba') {
+      myArr.sort((b, a) => a[sort].localeCompare(b[sort]));
+    }
+    return myArr;
+  };
+
+  // When lessons change filter data
+  useEffect(() => {
+    let localData = data;
+
+    if (filterType === 'type-ab')
+      localData = sortArray(localData, 'type', 'ab');
+
+    if (filterType === 'type-ba')
+      localData = sortArray(localData, 'type', 'ba');
+
+    if (filterType === 'pinyin-ab')
+      localData = sortArray(localData, 'pinyin', 'ab');
+
+    if (filterType === 'pinyin-ba')
+      localData = sortArray(localData, 'pinyin', 'ba');
+    // }
+    setSortData(localData);
+  }, [filterType, data]);
+
   return (
     <section>
-      <h1>New Words</h1>
+      <h1>New Words ({filterType})</h1>
+
       <div className="word word-header">
         <p>Hanzi</p>
-        <p>Pinyin</p>
+        <p
+          onClick={() => {
+            sortType('pinyin');
+          }}
+        >
+          Pinyin
+          {filterType === 'pinyin-ab' && <span>⬇️</span>}
+          {filterType === 'pinyin-ba' && <span>⬆️</span>}
+        </p>
         <p>Translation</p>
-        <p>Type</p>
+        <p
+          onClick={() => {
+            sortType('type');
+          }}
+        >
+          Type
+          {filterType === 'type-ab' && <span>⬇️</span>}
+          {filterType === 'type-ba' && <span>⬆️</span>}
+        </p>
       </div>
       <div className="word--list">
-        {data.map(word => {
-          return (
-            <div key={word.pinyin} className="word">
-              <p>{word.hanzi}</p>
-              <p>{word.pinyin}</p>
-              <p>{word.translation}</p>
-              <p>{word.type}</p>
-            </div>
-          );
-        })}
+        {sortData &&
+          sortData.map(word => {
+            return (
+              <div key={word.pinyin} className="word">
+                <p>{word.hanzi}</p>
+                <p>{word.pinyin}</p>
+                <p>{word.translation}</p>
+                <p>{word.type}</p>
+              </div>
+            );
+          })}
       </div>
     </section>
   );
