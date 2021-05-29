0b3f403d94dd09c14e5cdb2b125307336ba30472
Almer <48512743+meisterAlmer@users.noreply.github.com>
2021-05-29 10:30:24 +0200

    small update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import LoginContextProvider from './context/LoginContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+import LoginContextProvider from "./context/LoginContext";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,10 +10,10 @@ ReactDOM.render(
       <App />
     </LoginContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
